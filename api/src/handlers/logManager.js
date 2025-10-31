b0d89a6985bc79bf4c9406f4e75e243b7fafd47e
Luke Pereira <lukejoepereira@gmail.com>
2025-10-31 13:23:31 -0400

    Updates to support both *.tlog and *.bin flight logs

--- api/src/handlers/logManager.js
+++ api/src/handlers/logManager.js
@@ -1,6 +1,11 @@
 import fs from 'fs';
 import path from 'path';
-import * as dfd from 'danfojs-node';
+import * as ss from 'simple-statistics';
+import {
+  logDataMessageGroups,
+  messageDataDocs_bin,
+  messageDataDocs_tlog,
+} from './agent/prompts/logDocumentation.js';
 
 export class LogManager {
   static async uploadAndProcessLog(req, res) {
@@ -13,32 +18,144 @@ export class LogManager {
     // Files are stored inside a form using Deep Chat request FormData format:
     // https://deepchat.dev/docs/connect
     if (req.files) {
-      console.log('Files:');
-      console.log(req.files);
+      console.debug('Files:', req.files);
     }
-    const filepath = req.files[0].path;
-    await FileManager.postProcessLog(filepath);
     // Sends response back to Deep Chat using the Response format:
     // https://deepchat.dev/docs/connect/#Response
     res.status(200).json({ status: 'Files received successfully' });
+
+    // Process the uploaded log file in the background
+    const filepath = req.files[0].path;
+    await LogManager.preProcessLog(filepath);
   }
 
-  static getStatsForSeries(rawSeries) {
-    if (!rawSeries || rawSeries.length === 0) {
+  static getStatsForMessageField(messageFieldData, timeBootMsData = undefined) {
+    if (!messageFieldData || messageFieldData.length === 0) {
       return null;
     }
-    const df = new dfd.Series(rawSeries);
-    const describe = df.describe();
-    const keys = describe?.$index;
-    const values = describe?.$data;
-    const stats = {};
-    for (let i = 0; i < keys.length; i++) {
-      stats[keys[i]] = values[i];
+    let rawSeries = messageFieldData;
+
+    if (typeof messageFieldData === 'object' && !Array.isArray(messageFieldData)) {
+      rawSeries = Object.values(messageFieldData);
     }
+
+    if (!Array.isArray(rawSeries)) {
+      console.debug('Input is not an array');
+      return null;
+    }
+
+    const stats = {
+      count: rawSeries.length,
+      mean: ss.mean(rawSeries),
+      std: ss.standardDeviation(rawSeries),
+      min: ss.min(rawSeries),
+      max: ss.max(rawSeries),
+      median: ss.median(rawSeries),
+      q25: ss.quantile(rawSeries, 0.25),
+      q75: ss.quantile(rawSeries, 0.75),
+    };
+
+    if (!timeBootMsData) {
+      return stats;
+    }
+    // If time series data is provided, compute trend line and R²
+    let timeBootMsArray = timeBootMsData;
+    if (typeof timeBootMsData === 'object' && !Array.isArray(timeBootMsData)) {
+      timeBootMsArray = Object.values(timeBootMsData);
+    }
+    if (!Array.isArray(timeBootMsArray)) {
+      console.debug('timeBootMsData is not an array');
+      return stats;
+    }
+    if (timeBootMsArray.length !== rawSeries.length) {
+      console.debug('timeBootMsData length does not match messageFieldData length');
+      return stats;
+    }
+    const xy = timeBootMsArray.map((t, i) => [t, rawSeries[i]]);
+    const regression = ss.linearRegression(xy);
+    const line = ss.linearRegressionLine(regression);
+    const r2 = ss.rSquared(xy, line);
+
+    stats.trend = {
+      slope: regression.m,
+      intercept: regression.b,
+      r2: r2,
+    };
+
     return stats;
   }
 
-  static async postProcessLog(filepath) {
+  static getDescriptionForMessageField(logType, messageType, fieldKey) {
+    const messageDataDocs = logType === 'bin' ? messageDataDocs_bin : messageDataDocs_tlog;
+    let desc = `Placeholder description for ${messageType}.${fieldKey}`;
+    const messageDoc = messageDataDocs.find(
+      doc => doc.title === messageType || doc.id === messageType.toLowerCase(),
+    );
+
+    if (messageDoc) {
+      let fieldDoc = null;
+      for (const table of messageDoc.tables) {
+        const foundField = table.find(field => field.name === fieldKey);
+        if (foundField) {
+          fieldDoc = foundField;
+          break;
+        }
+      }
+
+      if (fieldDoc) {
+        desc = `${fieldDoc?.description} (Unit: ${fieldDoc?.unit})`;
+      } else {
+        desc = messageDoc.description || '';
+      }
+    }
+    return desc;
+  }
+
+  static getRawDataForMessageField(messageFieldData, timeBootMsData = undefined) {
+    if (!messageFieldData || messageFieldData.length === 0) {
+      return null;
+    }
+    let rawData = messageFieldData;
+    if (typeof messageFieldData === 'object' && !Array.isArray(messageFieldData)) {
+      rawData = Object.values(messageFieldData);
+    }
+
+    // If data repeats itself, only return unique value to reduce size
+    if (Array.isArray(rawData)) {
+      const uniqueData = Array.from(new Set(rawData));
+      if (uniqueData.length < rawData.length) {
+        return `${uniqueData} ... [truncated, ${rawData.length -
+          uniqueData.length} more repeated values]`;
+      }
+    }
+
+    // if timeBootMsData is provided, merge it with messageFieldData for time context
+    if (timeBootMsData) {
+      let timeBootMsArray = timeBootMsData;
+      if (typeof timeBootMsData === 'object' && !Array.isArray(timeBootMsData)) {
+        timeBootMsArray = Object.values(timeBootMsData);
+      }
+      if (!Array.isArray(timeBootMsArray)) {
+        console.debug('timeBootMsData is not an array');
+        return rawData;
+      }
+      if (timeBootMsArray.length !== rawData.length) {
+        console.debug('timeBootMsData length does not match messageFieldData length');
+        return rawData;
+      }
+
+      const combinedData = rawData.map((value, index) => {
+        return [`${timeBootMsArray[index]}ms`, value];
+      });
+      return combinedData;
+    }
+
+    return rawData;
+  }
+
+  static async preProcessLog(filepath) {
+    // Placeholder for any preprocessing steps if needed in the future
+    console.debug(`Preprocessing log file at ${filepath}`);
     // Read log file JSON from disk storage
     if (!filepath) {
       throw new Error(`No filename provided.`);
@@ -50,97 +167,44 @@ export class LogManager {
     }
     const fullLogDataRaw = fs.readFileSync(filePath, 'utf-8');
     const logData = JSON.parse(fullLogDataRaw);
+    const logType = logData?.logType;
+    if (!['tlog', 'bin'].includes(logType)) {
+      throw new Error(`Unsupported log type: ${logType}`);
+    }
 
-    // Process log data to extract statistics for various message types
-    const stats = {
-      SYSTEM_TIME: {
-        time_boot_ms: FileManager.getStatsForSeries(logData?.messages?.SYSTEM_TIME?.time_boot_ms),
-      },
-      GLOBAL_POSITION_INT: {
-        time_boot_ms: FileManager.getStatsForSeries(
-          logData?.messages?.GLOBAL_POSITION_INT?.time_boot_ms,
-        ),
-        lat: FileManager.getStatsForSeries(logData?.messages?.GLOBAL_POSITION_INT?.lat),
-        lon: FileManager.getStatsForSeries(logData?.messages?.GLOBAL_POSITION_INT?.lon),
-        alt: FileManager.getStatsForSeries(logData?.messages?.GLOBAL_POSITION_INT?.alt),
-        relative_alt: FileManager.getStatsForSeries(
-          logData?.messages?.GLOBAL_POSITION_INT?.relative_alt,
-        ),
-        vx: FileManager.getStatsForSeries(logData?.messages?.GLOBAL_POSITION_INT?.vx),
-        vy: FileManager.getStatsForSeries(logData?.messages?.GLOBAL_POSITION_INT?.vy),
-        vz: FileManager.getStatsForSeries(logData?.messages?.GLOBAL_POSITION_INT?.vz),
-        hdg: FileManager.getStatsForSeries(logData?.messages?.GLOBAL_POSITION_INT?.hdg),
-      },
-      GPS_RAW_INT: {
-        fix_type: FileManager.getStatsForSeries(logData?.messages?.GPS_RAW_INT?.fix_type),
-        lat: FileManager.getStatsForSeries(logData?.messages?.GPS_RAW_INT?.lat),
-        lon: FileManager.getStatsForSeries(logData?.messages?.GPS_RAW_INT?.lon),
-        alt: FileManager.getStatsForSeries(logData?.messages?.GPS_RAW_INT?.alt),
-        eph: FileManager.getStatsForSeries(logData?.messages?.GPS_RAW_INT?.eph),
-        epv: FileManager.getStatsForSeries(logData?.messages?.GPS_RAW_INT?.epv),
-        vel: FileManager.getStatsForSeries(logData?.messages?.GPS_RAW_INT?.vel),
-        cog: FileManager.getStatsForSeries(logData?.messages?.GPS_RAW_INT?.cog),
-        satellites_visible: FileManager.getStatsForSeries(
-          logData?.messages?.GPS_RAW_INT?.satellites_visible,
-        ),
-        alt_ellipsoid: FileManager.getStatsForSeries(logData?.messages?.GPS_RAW_INT?.alt_ellipsoid),
-        h_acc: FileManager.getStatsForSeries(logData?.messages?.GPS_RAW_INT?.h_acc),
-        v_acc: FileManager.getStatsForSeries(logData?.messages?.GPS_RAW_INT?.v_acc),
-        vel_acc: FileManager.getStatsForSeries(logData?.messages?.GPS_RAW_INT?.vel_acc),
-        hdg_acc: FileManager.getStatsForSeries(logData?.messages?.GPS_RAW_INT?.hdg_acc),
-        yaw: FileManager.getStatsForSeries(logData?.messages?.GPS_RAW_INT?.yaw),
-        time_boot_ms: FileManager.getStatsForSeries(logData?.messages?.GPS_RAW_INT?.time_boot_ms),
-      },
-      HEARTBEAT: {
-        type: FileManager.getStatsForSeries(logData?.messages?.HEARTBEAT?.type),
-        autopilot: FileManager.getStatsForSeries(logData?.messages?.HEARTBEAT?.autopilot),
-        base_mode: FileManager.getStatsForSeries(logData?.messages?.HEARTBEAT?.base_mode),
-        custom_mode: FileManager.getStatsForSeries(logData?.messages?.HEARTBEAT?.custom_mode),
-        system_status: FileManager.getStatsForSeries(logData?.messages?.HEARTBEAT?.system_status),
-        mavlink_version: FileManager.getStatsForSeries(
-          logData?.messages?.HEARTBEAT?.mavlink_version,
-        ),
-        time_boot_ms: FileManager.getStatsForSeries(logData?.messages?.HEARTBEAT?.time_boot_ms),
-      },
-      ATTITUDE: {
-        time_boot_ms: FileManager.getStatsForSeries(logData?.messages?.ATTITUDE?.time_boot_ms),
-        roll: FileManager.getStatsForSeries(logData?.messages?.ATTITUDE?.roll),
-        pitch: FileManager.getStatsForSeries(logData?.messages?.ATTITUDE?.pitch),
-        yaw: FileManager.getStatsForSeries(logData?.messages?.ATTITUDE?.yaw),
-        rollspeed: FileManager.getStatsForSeries(logData?.messages?.ATTITUDE?.rollspeed),
-        pitchspeed: FileManager.getStatsForSeries(logData?.messages?.ATTITUDE?.pitchspeed),
-        yawspeed: FileManager.getStatsForSeries(logData?.messages?.ATTITUDE?.yawspeed),
-      },
-      AHRS: {
-        omegaIx: FileManager.getStatsForSeries(logData?.messages?.AHRS?.omegaIx),
-        omegaIy: FileManager.getStatsForSeries(logData?.messages?.AHRS?.omegaIy),
-        omegaIz: FileManager.getStatsForSeries(logData?.messages?.AHRS?.omegaIz),
-        accel_weight: FileManager.getStatsForSeries(logData?.messages?.AHRS?.accel_weight),
-        renorm_val: FileManager.getStatsForSeries(logData?.messages?.AHRS?.renorm_val),
-        error_rp: FileManager.getStatsForSeries(logData?.messages?.AHRS?.error_rp),
-        error_yaw: FileManager.getStatsForSeries(logData?.messages?.AHRS?.error_yaw),
-        time_boot_ms: FileManager.getStatsForSeries(logData?.messages?.AHRS?.time_boot_ms),
-      },
-      AHRS2: {},
-      AHRS3: {},
-      PARAM_VALUE: {
-        param_value: FileManager.getStatsForSeries(logData?.messages?.PARAM_VALUE?.param_value),
-        param_type: FileManager.getStatsForSeries(logData?.messages?.PARAM_VALUE?.param_type),
-        param_count: FileManager.getStatsForSeries(logData?.messages?.PARAM_VALUE?.param_count),
-        param_index: FileManager.getStatsForSeries(logData?.messages?.PARAM_VALUE?.param_index),
-        time_boot_ms: FileManager.getStatsForSeries(logData?.messages?.PARAM_VALUE?.time_boot_ms),
-      },
-      STATUSTEXT: {
-        severity: FileManager.getStatsForSeries(logData?.messages?.STATUSTEXT?.severity),
-        time_boot_ms: FileManager.getStatsForSeries(logData?.messages?.STATUSTEXT?.time_boot_ms),
-      },
-      trajectories: {},
-    };
-    
-    // Save stats as JSON file 
+    const logStats = {};
+
+    for (const [groupName, groupInfo] of Object.entries(logDataMessageGroups)) {
+      const validMessages = logType === 'tlog' ? groupInfo.tlog : groupInfo.bin;
+      if (!validMessages) continue;
+
+      logStats[groupName] = {};
+
+      for (const messageType of validMessages) {
+        const messageData = logData?.messages?.[messageType];
+        if (!messageData) continue;
+
+        const dataKeys = Object.keys(messageData);
+        logStats[groupName][messageType] = dataKeys.map(key => ({
+          data_key: key,
+          description: LogManager.getDescriptionForMessageField(logType, messageType, key),
+          stats: LogManager.getStatsForMessageField(
+            messageData[key],
+            messageData?.['time_boot_ms'],
+          ),
+          rawData: LogManager.getRawDataForMessageField(
+            messageData[key],
+            messageData?.['time_boot_ms'],
+          ),
+        }));
+      }
+    }
+
+    // Save stats and doc contexts as JSON files
     const statsFilename = `stats_${path.basename(filepath)}`;
     const statsFilePath = path.resolve(path.dirname(filepath), statsFilename);
-    fs.writeFileSync(statsFilePath, JSON.stringify(stats, null, 2), 'utf-8');
+    fs.writeFileSync(statsFilePath, JSON.stringify(logStats, null, 2), 'utf-8');
     console.log(`Stats saved to ${statsFilePath}`);
+    return;
   }
 }
